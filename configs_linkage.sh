5f88622627a92bf26d3589740cbd61b40cade6e7
Jon Austin <jonaustin@jonaustin.org>
2010-07-19 05:41:03 +0200

    added (very) simple shell script to add links to main configs

---
+++ configs_linkage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ln configs/.bashrc 
+ln configs/.Xdefaults
+ln -s configs/.vim
+ln configs/.vimrc
+ln configs/.gitconfig
+ln configs/.vimperatorrc
+ln -s configs/.vimperator
+ln configs/.screenrc
+ln configs/.inputrc 
