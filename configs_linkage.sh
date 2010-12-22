1efc6c514f8583035bca1c016ce5d1bb9f7aadd0
Jon Austin <jonaustin@jonaustin.org>
2010-12-21 22:11:41 -0800

    added .synergy.conf, updated configs_linkage

--- configs_linkage.sh
+++ configs_linkage.sh
@@ -8,10 +8,23 @@ ln -s configs/.vimperatorrc
 ln -s configs/.vimperator
 ln -s configs/.screenrc
 ln -s configs/.inputrc 
-ln -s configs/.config/awesome .config/awesome
-ln -s configs/.config/vimprobable .config/vimprobable
+ln -s /home/jon/configs/.config/awesome /home/jon/.config/awesome
+ln -s /home/jon/configs/.config/vimprobable /home/jon/.config/vimprobable
+ln -s configs/.elinks/ .
+ln -s configs/.gemrc .
+ln -s configs/.irbrc .
+ln -s configs/.ncmpcpp/ .
+ln -s configs/.rtorrent.rc .
+ln -s configs/.xinitrc .
+
 mkdir .vim/bin
 ln -s ${HOME}/.vim/bundle/vim-ruby-debugger/vim/bin/ruby_debugger.rb .vim/bin/
 ln -s ${HOME}/.vim/bundle/vim-ruby-debugger/vim/plugin/ruby_debugger.vim .vim/plugin/
 ln -s ${HOME}/.vim/bundle/vim-ruby-debugger/vim/doc/ruby_debugger.txt .vim/doc/ruby_debugger.txt
+
+# optional
+#ln -s configs/.asoundrc .
+#ln -s configs/.inputrc .
+#ln -s configs/.xbindkeysrc .
+#ln -s configs/.xmodmap 
 
