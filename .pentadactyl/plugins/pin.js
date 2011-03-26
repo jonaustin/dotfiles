d335dbd9e51b2a9c3f01ff2652a7bd64d1e910c3
Jon Austin <jonaustin@jonaustin.org>
2011-03-26 15:24:45 -0700

    adding pinboard commands for pentadactyl

---
+++ .pentadactyl/plugins/pin.js
@@ -0,0 +1,23 @@
+// Vimperator Plugin: 'pinboard'
+// Last Change: 22-Jan-2011
+// License: MIT
+// Maintainer: Jon Austin <jon@jonaustin.org>
+// URL: 
+// Usage: Use :pin <tags delimited by spaces> command
+// Usage: if successfully posted you will see "done" echoed
+
+group.commands.add(['pin'], "Save page as a bookmark on pinboard",
+                        function(args) {
+                            var url = "https://api.pinboard.in/v1/posts/add?";
+                            url += "&url=" + encodeURIComponent(buffer.URL);
+                            url += "&description=" + encodeURIComponent(buffer.title);
+                            url += "&tags=" + encodeURIComponent(args.string);
+                            
+                            var xhr = new XMLHttpRequest();
+                            xhr.open("POST", url, false);
+                            xhr.send(null);
+                            var xml = (new DOMParser()).parseFromString(xhr.responseText, "text/xml");
+                            var status = xml.getElementsByTagName('result')[0].getAttribute('code');
+                            
+                            dactyl.echo(status);
+                        });
