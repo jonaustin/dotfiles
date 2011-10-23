3a51b7ce7e5882463821a14b527c7721e4880b19
Jon Austin <jonaustin@jonaustin.org>
2011-10-23 11:58:41 -0700

    deleting old awesome config

--- .config/awesome/bashets/mem.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-TOTAL=`free | grep Mem | awk '{print $2}'`
-USED=`free | grep - | awk '{print $3}'`
-let PERCC=$USED*100/$TOTAL
-echo -n $PERCC
--- .config/awesome/bashets/mpd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-STATUS=`/usr/share/awesome/bashets/mpd.awk`
-TITLE=`mpc -p 6600 | head -n 1`
-MTIME=`mpc -p 6600 | head -n 2 | tail -n 1 | awk '{print $3}'`
-
-echo -n "$STATUS|$TITLE|$MTIME"
