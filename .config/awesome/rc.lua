46e21e03f391e36cc4d162073a713a8b2af31076
Jon Austin <jon.i.austin@gmail.com>
2012-07-18 22:53:33 -0700

    remove window borders

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -401,7 +401,7 @@ root.keys(globalkeys)
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
+      properties = { border_width = 0, --beautiful.border_width,
                      border_color = beautiful.border_normal,
                      focus = true,
                      keys = clientkeys,
