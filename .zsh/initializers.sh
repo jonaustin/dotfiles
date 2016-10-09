193518a9ba22ef90d41928879998707b588ef0a1
Jon Austin <jon.i.austin@gmail.com>
2016-10-09 10:03:26 -0700

    not needed with osx - launchd manages
    http://bridge.grumpy-troll.org/2011/07/diversion-into-macosx-launchd--ssh-agent/

--- .zsh/initializers.sh
+++ .zsh/initializers.sh
@@ -1,26 +1,6 @@
 ## tmuxinator
 [[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
 
-# SSH
-# ssh-agent
-SSH_ENV="$HOME/.ssh/environment"
-start_agent() {
-  echo "Initialising new SSH agent..."
-  /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-  echo 'succeeded'
-  chmod 600 "${SSH_ENV}"
-  . "${SSH_ENV}" > /dev/null
-  /usr/bin/ssh-add;
-}
-# Source SSH settings, if applicable
-if [ -f "${SSH_ENV}" ]; then
-  . "${SSH_ENV}" > /dev/null
-  #ps ${SSH_AGENT_PID}
-  ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null ||  start_agent;
-else
-  start_agent;
-fi
-
 # fasd
 #eval "$(--init zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)"
 #alias f='fasd -f'        # file
