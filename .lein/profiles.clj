995abe3164ca413dd9ea4e386aaf89af4086151f
Jon Austin <jon.i.austin@gmail.com>
2017-05-21 13:00:10 -0700

    various

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
-{:user {:plugins      [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                       [refactor-nrepl "1.2.0-SNAPSHOT"]
+{:user {:plugins      [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
+                       [refactor-nrepl "2.3.0-SNAPSHOT"]
                        [lein-exec "0.3.6"]]
         :dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.10"]]}}
+                       [org.clojure/tools.nrepl "0.2.12"]]}}
