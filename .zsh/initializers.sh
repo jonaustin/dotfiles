a0694239f23902f508241ea85a875cc31e70e68a
Jon Austin <jon.i.austin@gmail.com>
2018-04-23 18:58:47 -0700

    no need to run every time

--- .zsh/initializers.sh
+++ .zsh/initializers.sh
@@ -85,22 +85,6 @@ if [ $SYSTEM_TYPE = "Darwin" ]; then
 else
   source /usr/share/zsh/share/antigen.zsh
 fi
-# Bundles from the default repo (robbyrussell's oh-my-zsh).
-antigen bundle git
-antigen bundle heroku
-antigen bundle pip
-antigen bundle lein
-#antigen bundle command-not-found
-
-# Syntax highlighting bundle.
-antigen bundle zsh-users/zsh-syntax-highlighting
-
-# Load the theme.
-#antigen theme pure #FIXME: use my custom version
-
-# Tell Antigen that you're done.
-antigen apply
-###
 
 ## Other
 
