a7ae348f191f0160ef8306a02731701dda1f5a39
Jon Austin <jon.i.austin@gmail.com>
2024-01-20 22:41:49 -0800

    opts

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,6 +12,38 @@ vim.opt.breakindent = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = false
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.o.clipboard = 'unnamedplus'
+vim.o.autoindent = true -- indent at the same level of the previous line
+vim.o.wrap = true -- wrap long lines
+
+-- Enable break indent
+vim.o.breakindent = true
+
+vim.o.undofile = true
+vim.o.backup = true
+vim.o.backupdir="$HOME/.vimbackup"
+vim.o.directory="$HOME/.vimswap"
+vim.o.viewdir="$HOME/.vimviews"
+vim.o.undodir="$HOME/.vimundo"
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
 
 
 -- ========================================================================== --
@@ -99,8 +131,8 @@ lazy.path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 lazy.opts = {}
 
 lazy.setup({
-	-- coding
-	'fatih/vim-go',
+  -- coding
+  'fatih/vim-go',
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb', -- :GBrowse
@@ -108,16 +140,16 @@ lazy.setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
-	-- colorschemes
+  -- colorschemes
   'folke/tokyonight.nvim',
 
   'kyazdani42/nvim-web-devicons',
   'nvim-lualine/lualine.nvim',
-	'nvim-lua/plenary.nvim',
-	'majutsushi/tagbar', 
-	'github/copilot.vim',
+  'nvim-lua/plenary.nvim',
+  'majutsushi/tagbar',
+  'github/copilot.vim',
 
-	-- kickstart
+  -- kickstart
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
@@ -147,7 +179,7 @@ lazy.setup({
 
       -- Adds LSP completion capabilities
       'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-path', -- completion for local files
 
       -- Adds a number of user-friendly snippets
       'rafamadriz/friendly-snippets',
@@ -240,13 +272,8 @@ lazy.setup({
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
       {
         'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
         build = 'make',
         cond = function()
           return vim.fn.executable 'make' == 1
@@ -282,42 +309,6 @@ lazy.setup({
 -- [[ Setting options ]]
 -- See `:help vim.o`
 
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
 
 -- [[ Basic Keymaps ]]
 
