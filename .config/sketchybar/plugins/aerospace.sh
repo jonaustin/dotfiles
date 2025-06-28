5a76ca58176a881a1137bab2157c3766587e95e8
Jon Austin <jon.i.austin@gmail.com>
2025-06-28 13:38:37 -0700

    sketchybar

---
+++ .config/sketchybar/plugins/aerospace.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# make sure it's executable with:
+# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
+
+# if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+#     echo $NAME > /tmp/sketch
+#     sketchybar --set $NAME background.drawing=on
+# else
+#     echo $NAME > /tmp/sketch
+#     sketchybar --set $NAME background.drawing=off
+# fi
+
+if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+  sketchybar --set $NAME background.color=0x44FFFFFF label.shadow.drawing=off icon.shadow.drawing=off background.border_width=2
+else
+  sketchybar --set $NAME background.color=0x44FFFFFF label.shadow.drawing=off icon.shadow.drawing=off background.border_width=0
+fi
